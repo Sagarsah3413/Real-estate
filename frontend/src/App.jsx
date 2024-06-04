017c40192a000880ce64d6252748b2ce56d3b5a2
Sagarsah3413 <sahs82341@gmail.com>
2024-06-05 00:09:02 +0530

    create and run server

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./assets/pages/Home";
+import Signin from "./assets/pages/Signin";
+import Profile from "./assets/pages/Profile";
+import About from "./assets/pages/About";
+import Signup from "./assets/pages/Signup";
+import Header from "./assets/components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin/>} />
+        <Route path="/sign-up" element={<Signup/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
